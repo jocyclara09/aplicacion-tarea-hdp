325c21e11ede299d0a4943749e709e2a8de06326
jocyclara09 <jocyclara09@gmail.com>
2017-06-24 00:18:25 -0600

    Agregando vista para encuestas.

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.lista_encuesta),
+    url(r'^encuesta/(?P<pk>[0-9]+)/$', views.detalle_encuesta),
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Encuesta
 
 def lista_encuesta(request):
 	encuestas = Encuesta.objects.filter(fecha_creacion__lte=timezone.now()).order_by('fecha_creacion')
 	return render(request, 'aplicacion/lista_encuesta.html', {'encuestas': encuestas})
+
+def detalle_encuesta(request, pk):
+    encuesta = get_object_or_404(Encuesta, pk=pk)
+    return render(request, 'aplicacion/detalle_encuesta.html', {'encuesta': encuesta})
