a64f35bcc0caf25bafccd45ee4738578a2880315
jocyclara09 <jocyclara09@gmail.com>
2017-07-03 17:08:51 -0600

    Añadiendo exitosamente PyR a la Base

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Encuesta
+from .models import Encuesta, Pregunta, Respuesta
+
 
 admin.site.register(Encuesta)
+admin.site.register(Pregunta)
+admin.site.register(Respuesta)
+
+
